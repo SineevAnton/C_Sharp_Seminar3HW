bb6f940f7b254afae458fa4fa932f65f277cc51c
Anton Sineev <sineevanton@mail.ru>
2022-05-15 17:54:28 +0300

    Task23 complete

---
+++ Task23/Task23.cs
@@ -0,0 +1,15 @@
+﻿//Write a programm which will write the cubes of digits from 1 to input number.
+
+Console.Write("Enter any number: ");
+int num = int.Parse(Console.ReadLine());
+
+void printCubes(int n)
+{
+    Console.WriteLine("|{0,8}|{1,8}|", "Number", "Cube");
+    for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine("|{0,8}|{1,8}|", i, Math.Pow(i, 3));
+    }
+}
+
+printCubes(num);
