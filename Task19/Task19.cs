91326d966aef1345c192ee736a9f06acf06bdef6
Anton Sineev <sineevanton@mail.ru>
2022-05-15 17:37:31 +0300

    Task 19 complete

---
+++ Task19/Task19.cs
@@ -0,0 +1,19 @@
+﻿//Write a program which checkes if the inputed five-digit number is a palindrome.
+
+string num;
+while (true)
+{
+    Console.Write("Enter a five-digit number: ");
+    string text = Console.ReadLine();
+    if (int.TryParse(text, out int number) && number < 100000 && number > 9999) //checking input number
+    {
+        num = text;
+        break;
+    }
+    Console.WriteLine("Wrong input. Enter a five-digit number: ");
+}
+
+char[] numReverse = num.ToCharArray();
+Array.Reverse(numReverse);
+string result = new string(numReverse);
+Console.WriteLine(num == result ? "Yes" : "No");
